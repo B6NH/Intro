7297d8693215c6c9a170f32340dd5ce09e7a1734
B6NH <B6NH@users.noreply.github.com>
2023-01-17 19:20:54 +0100

    Initialize Repository

---
+++ rot13.m
@@ -0,0 +1,66 @@
+
+% Rot 13
+
+:- module rot13.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+:- implementation.
+:- import_module char, list, string.
+
+:- func rot13(char) = char.
+
+rot13(Char) =
+  (  if Char = 'a' then 'n'
+     else if Char = 'b' then 'o'
+     else if Char = 'c' then 'p'
+     else if Char = 'd' then 'q'
+     else if Char = 'e' then 'r'
+     else Char
+  ).
+
+:- func rot13_2(char) = char.
+
+rot13_2(CharIn) = ( if prot(CharIn, CharOut) then CharOut else CharIn ).
+
+:- pred prot(char::in, char::out) is semidet.
+
+prot('a','n').
+prot('b','o').
+prot('c','p').
+prot('d','q').
+prot('e','r').
+
+:- pred mloop(io::di, io::uo) is det.
+
+mloop(!IO) :-
+
+  % Read character
+  io.read_char(Result, !IO),
+
+  (
+
+     % Show rotated character
+     Result = ok(Char),
+     io.write_char(rot13_2(Char), !IO),
+
+     % Next character
+     mloop(!IO)
+
+  ;
+
+     % Exit
+     Result = eof
+
+  ;
+
+     % Error
+     Result = error(ErrorCode),
+     io.format("%s\n", [s(io.error_message(ErrorCode))], !IO)
+
+  ).
+
+main(!IO) :-
+  io.format("%s %c\n", [s("A:"), c(rot13('a'))], !IO),
+  mloop(!IO).
+
